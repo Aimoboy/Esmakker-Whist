bc93d9df7d3cdc6d3673bf8c9f376f114cffd640
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2020-03-25 22:15:12 +0100

    Added Call class and ClassType enum

---
+++ Game/GameModel/Call.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.GameModel {
+    public class Call {
+        public CallType Type { get; set; }
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Call constructor for types: None, Vip, Gode, Halve, Sang
+        /// </summary>
+        /// <param name="type">The type of call</param>
+        /// <param name="number">The call number</param>
+        public Call(CallType type, int number) {
+            switch (type) {
+                case CallType.Pass:
+                case CallType.Sol:
+                case CallType.RenSol:
+                case CallType.Bordlægger:
+                case CallType.SuperBordlægger:
+                    throw new ArgumentException();
+            }
+
+            if (number < 7 || number > 13) {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Type = type;
+            Number = number;
+        }
+
+        /// <summary>
+        /// Call constructor for types: Pass, Sol, RenSol, Bordlægger, SuperBordlægger
+        /// </summary>
+        /// <param name="type">The type of call</param>
+        public Call(CallType type) {
+            Type = type;
+
+            Number = type switch
+            {
+                CallType.Pass => 7,
+                CallType.Sol => 9,
+                CallType.RenSol => 10,
+                CallType.Bordlægger => 11,
+                CallType.SuperBordlægger => 12,
+                _ => throw new ArgumentException();
+            };
+        }
+    }
+}
---
+++ Game/GameModel/CallType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.GameModel {
+    public enum CallType {
+        Pass,
+        None,
+        Sol,
+        RenSol,
+        Bordlægger,
+        SuperBordlægger,
+        Vip,
+        Gode,
+        Halve,
+        Sang
+    }
+}
