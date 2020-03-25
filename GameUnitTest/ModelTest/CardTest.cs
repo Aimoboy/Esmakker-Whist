bf83eba0cef929cc4954c3baecccab637b0807cc
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2020-03-25 21:54:32 +0100

    Created unit test project and test for card creation

---
+++ GameUnitTest/ModelTest/CardTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Game.GameModel;
+
+namespace GameUnitTest.ModelTest {
+    [TestFixture]
+    public class CardTest {
+        [TestCase(CardType.Spades)]
+        [TestCase(CardType.Clubs)]
+        [TestCase(CardType.Hearts)]
+        [TestCase(CardType.Diamonds)]
+        public void CardCreationBorderCase(CardType type) {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Card(type, 1));
+            Assert.DoesNotThrow(() => new Card(type, 2));
+            Assert.DoesNotThrow(() => new Card(type, 14));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Card(type, 15));
+        }
+    }
+}
