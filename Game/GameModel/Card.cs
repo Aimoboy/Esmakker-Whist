e2b26e0b64494c1d9b2f3d61fce204e30d70e7a7
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2020-03-25 21:53:23 +0100

    Created Card class and CardType enum

---
+++ Game/GameModel/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.GameModel {
+    public class Card {
+        public CardType Type { get; set; }
+        public int Number { get; set; }
+
+        public Card(CardType type, int number) {
+            if (number < 2 || number > 14) {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Type = type;
+            Number = number;
+        }
+    }
+}
---
+++ Game/GameModel/CardType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.GameModel {
+    public enum CardType {
+        Spades,
+        Clubs,
+        Hearts,
+        Diamonds
+    }
+}
