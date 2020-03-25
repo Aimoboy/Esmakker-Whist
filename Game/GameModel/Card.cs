15e60b1b8d59fac7232341a27e5f2673c20bcb27
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2020-03-25 22:14:40 +0100

    Added documentation to Card class

--- Game/GameModel/Card.cs
+++ Game/GameModel/Card.cs
@@ -7,6 +7,11 @@ namespace Game.GameModel {
         public CardType Type { get; set; }
         public int Number { get; set; }
 
+        /// <summary>
+        /// Card constructor
+        /// </summary>
+        /// <param name="type">The card type</param>
+        /// <param name="number">The card number</param>
         public Card(CardType type, int number) {
             if (number < 2 || number > 14) {
                 throw new ArgumentOutOfRangeException();
