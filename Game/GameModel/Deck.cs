2da54c4ee5473ae9c53afe628878839fc7d50caf
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2020-03-26 03:40:49 +0100

    Created a Deck class with GenerateCards and Shuffle as well as tests for this

---
+++ Game/GameModel/Deck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.GameModel {
+    public class Deck {
+        public Card[] Cards { get; set; } = new Card[55];
+
+        /// <summary>
+        /// Generate 55 cards for the card array in the deck
+        /// </summary>
+        public void GenerateCards() {
+            CardType[] types = { CardType.Spades, CardType.Clubs, CardType.Hearts, CardType.Diamonds };
+
+            for (int i = 0; i < types.Length; i++) {
+                for (int j = 0; j < 13; j++) {
+                    Cards[j + i * 13] = new Card(types[i], j + 2);
+                }
+            }
+
+            for (int i = 52; i < 55; i++) {
+                Cards[i] = new Card(CardType.Joker);
+            }
+        }
+
+        /// <summary>
+        /// Shuffle the cards
+        /// </summary>
+        /// <param name="iterations">The number of times to switch two random cards</param>
+        public void Shuffle(int iterations) {
+            Random rand = new Random();
+
+            for (int i = 0; i < iterations; i++) {
+                int i1 = rand.Next(Cards.Length);
+                int i2 = rand.Next(Cards.Length);
+
+                Card tmp = Cards[i1];
+                Cards[i1] = Cards[i2];
+                Cards[i2] = tmp;
+            }
+        }
+    }
+}
---
+++ GameUnitTest/ModelTest/DeckTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Game.GameModel;
+
+namespace GameUnitTest.ModelTest {
+    [TestFixture]
+    public class DeckTest {
+        Deck deck;
+
+        [SetUp]
+        public void SetUp() {
+            deck = new Deck();
+            deck.GenerateCards();
+        }
+
+        [Test]
+        public void TestCardGenerationNoNull() {
+            foreach (Card card in deck.Cards) {
+                Assert.IsNotNull(card);
+            }
+        }
+
+        [Test]
+        public void TestCardGenerationOneOfEach() {
+            Assert.IsTrue(OneOfEach());
+        }
+
+        [Test]
+        public void TestShuffleOneOfEach() {
+            deck.Shuffle(100);
+            Assert.IsTrue(OneOfEach());
+        }
+
+        
+        public bool OneOfEach() {
+            CardType[] types = { CardType.Spades, CardType.Clubs, CardType.Hearts, CardType.Diamonds };
+
+            bool result = true;
+            for (int i = 0; i < types.Length; i++) {
+                for (int j = 0; j < 13; j++) {
+
+                    bool found = false;
+                    Card c = new Card(types[i], j + 2);
+                    foreach (Card card in deck.Cards) {
+                        if (card == c) {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        result = false;
+                    }
+
+                }
+            }
+
+            int jokerCount = 0;
+            foreach (Card card in deck.Cards) {
+                if (card.Type == CardType.Joker) {
+                    jokerCount++;
+                }
+            }
+
+            return result && jokerCount == 3;
+        }
+    }
+}
